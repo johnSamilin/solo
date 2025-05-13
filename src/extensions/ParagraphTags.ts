dc0953b67dcf7999f3a224a87b3ccedd3a8a9f50
johnSamilin <temp.kroogi@gmail.com>
2025-05-13 13:13:22 +0300

    Fixed paragraph tags

--- src/extensions/ParagraphTags.ts
+++ src/extensions/ParagraphTags.ts
@@ -2,6 +2,21 @@ import { Extension } from '@tiptap/core';
 import { Plugin, PluginKey } from '@tiptap/pm/state';
 import { Decoration, DecorationSet } from '@tiptap/pm/view';
 
+declare module '@tiptap/core' {
+  interface Commands<ReturnType> {
+    paragraphTags: {
+      /**
+       * Set tags for the current paragraph
+       */
+      setParagraphTags: (tags: string[]) => ReturnType;
+      /**
+       * Remove tags from the current paragraph
+       */
+      removeParagraphTags: () => ReturnType;
+    };
+  }
+}
+
 export const ParagraphTags = Extension.create({
   name: 'paragraphTags',
 
@@ -11,29 +26,68 @@ export const ParagraphTags = Extension.create({
     };
   },
 
-  addAttributes() {
-    return {
-      tags: {
-        default: [],
-        parseHTML: element => {
-          const tags = element.getAttribute('data-tags');
-          return tags ? tags.split(',') : [];
-        },
-        renderHTML: attributes => {
-          if (!attributes.tags?.length) return {};
-          return { 'data-tags': attributes.tags.join(',') };
+  addGlobalAttributes() {
+    return [
+      {
+        types: ['paragraph'],
+        attributes: {
+          tags: {
+            default: [],
+            parseHTML: element => {
+              const tags = element.getAttribute('data-tags');
+              return tags ? tags.split(',') : [];
+            },
+            renderHTML: attributes => {
+              if (!attributes.tags?.length) return {};
+              return { 'data-tags': attributes.tags.join(',') };
+            },
+          },
         },
       },
-    };
+    ];
   },
 
   addCommands() {
     return {
-      setParagraphTags: (tags: string[]) => ({ commands }) => {
-        return commands.updateAttributes('paragraph', { tags });
+      setParagraphTags: (tags: string[]) => ({ tr, dispatch }) => {
+        const { selection } = tr;
+        const { $from } = selection;
+        
+        // Find the paragraph node
+        const depth = $from.depth;
+        const pos = $from.before(depth);
+        const node = $from.node(depth);
+
+        if (node.type.name !== 'paragraph') return false;
+
+        if (dispatch) {
+          // Update the node's attributes
+          tr.setNodeMarkup(pos, undefined, {
+            ...node.attrs,
+            tags,
+          });
+        }
+
+        return true;
       },
-      removeParagraphTags: () => ({ commands }) => {
-        return commands.resetAttributes('paragraph', 'tags');
+      removeParagraphTags: () => ({ tr, dispatch }) => {
+        const { selection } = tr;
+        const { $from } = selection;
+        
+        const depth = $from.depth;
+        const pos = $from.before(depth);
+        const node = $from.node(depth);
+
+        if (node.type.name !== 'paragraph') return false;
+
+        if (dispatch) {
+          tr.setNodeMarkup(pos, undefined, {
+            ...node.attrs,
+            tags: [],
+          });
+        }
+
+        return true;
       },
     };
   },
