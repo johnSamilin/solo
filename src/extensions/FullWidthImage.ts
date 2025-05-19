0045d2141a5ef97248e362518f4eb14524c271b7
Alex Saltykov <johnSamilin@users.noreply.github.com>
2025-05-19 11:54:31 +0300

    Add TipTap extension for full-width images

---
+++ src/extensions/FullWidthImage.ts
@@ -0,0 +1,54 @@
+import { Extension } from '@tiptap/core';
+import { Plugin, PluginKey } from '@tiptap/pm/state';
+import { Decoration, DecorationSet } from '@tiptap/pm/view';
+
+export const FullWidthImage = Extension.create({
+  name: 'fullWidthImage',
+
+  addGlobalAttributes() {
+    return [
+      {
+        types: ['image'],
+        attributes: {
+          isFullWidth: {
+            default: false,
+            parseHTML: element => element.classList.contains('full-width'),
+            renderHTML: attributes => {
+              if (!attributes.isFullWidth) {
+                return {};
+              }
+
+              return {
+                class: 'full-width',
+              };
+            },
+          },
+        },
+      },
+    ];
+  },
+
+  addCommands() {
+    return {
+      toggleImageFullWidth:
+        () =>
+        ({ tr, dispatch }) => {
+          const { selection } = tr;
+          const node = tr.doc.nodeAt(selection.from);
+
+          if (!node || node.type.name !== 'image') {
+            return false;
+          }
+
+          if (dispatch) {
+            tr.setNodeMarkup(selection.from, null, {
+              ...node.attrs,
+              isFullWidth: !node.attrs.isFullWidth,
+            });
+          }
+
+          return true;
+        },
+    };
+  },
+});
