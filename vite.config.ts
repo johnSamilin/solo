ee07a81941d9de0d387cb8ef54c8ad29a061f44b
Alex Saltykov <johnSamilin@users.noreply.github.com>
2025-07-21 13:08:37 +0300

    Fix icon paths in manifest and build config

--- vite.config.ts
+++ vite.config.ts
@@ -22,12 +22,12 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/assets/icons/png/192x192.png',
+            src: '/icons/192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/assets/icons/png/512x512.png',
+            src: '/icons/512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
