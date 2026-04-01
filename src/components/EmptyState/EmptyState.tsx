dc340bd962b7e65aa287dce39967526cbdac3348
Alex Saltykov <johnSamilin@users.noreply.github.com>
2026-04-01 10:14:53 +0300

    Updated EmptyState.tsx

--- src/components/EmptyState/EmptyState.tsx
+++ src/components/EmptyState/EmptyState.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState, useMemo } from 'react';
+import React, { FC, useState, useMemo, useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
 import MeshGradient from 'mesh-gradient.js';
 import { Plus, ChevronRight, ChevronDown } from 'lucide-react';
